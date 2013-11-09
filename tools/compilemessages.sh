5061c92e61159819d9974bb3cc308e500c9656f6
Jan Čermák <jan.cermak@nic.cz>
2013-11-09 20:12:22 +0100

    tools for making/compiling messages are printing info about progress

--- tools/compilemessages.sh
+++ tools/compilemessages.sh
@@ -3,5 +3,7 @@ for lang_dir in locale/*; do
 	locale_dir=$lang_dir/LC_MESSAGES/
 	for file in $locale_dir/*.po; do
 		msgfmt $file -o ${file%.*}.mo
+		echo "Compiling messages in $locale_dir."
 	done
 done
+echo "All messages compiled."
--- tools/makemessages.sh
+++ tools/makemessages.sh
@@ -11,11 +11,13 @@ then
 		xgettext -d messages --no-location --language=Python --from-code=UTF-8 --output=$locale_dir/tmp_messages.pot -
 	if [ -f $locale_dir/messages.po ]
 	then
+		echo "Making messages in $locale_dir."
 		msgmerge -q -U $locale_dir/messages.po $locale_dir/tmp_messages.pot
 		rm $locale_dir/tmp_messages.pot
 	else
 		mv $locale_dir/tmp_messages.pot $locale_dir/messages.po
 	fi
+	echo "Message making completed."
 else
-	echo "No language specified - use 'makemessages LANGUAGE'"
+	echo "No language specified - use '$0 LANGUAGE'"
 fi
