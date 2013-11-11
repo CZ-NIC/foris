0fece7d779f6d7a6eeb538b398efa6331a6cd0bd
Jan Čermák <jan.cermak@nic.cz>
2013-11-11 16:27:47 +0100

    factory for uci config filters

--- nuci/filters.py
+++ nuci/filters.py
@@ -7,6 +7,18 @@ from nuci.modules import time, uci_raw, updater
 import xml.etree.cElementTree as ET
 
 
+# top-level containers
 uci = ET.Element(uci_raw.Uci.qual_tag("uci"))
 updater = ET.Element(updater.Updater.qual_tag("updater"))
 time = ET.Element(time.Time.qual_tag("time"))
+
+
+# factory for uci configs
+def create_uci_config(config_name):
+    _uci = uci_raw.Uci()
+    _config = uci_raw.Config(config_name)
+    _uci.add(_config)
+    return _config.get_tree()
+
+
+foris_config = create_uci_config("foris")
