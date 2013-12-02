68e3904398b43b61cf3526cd6d9cd7167955ca4a
Jan Čermák <jan.cermak@nic.cz>
2013-12-02 13:51:52 +0100

    added licensing information - closes #2816

--- tools/compilemessages.sh
+++ tools/compilemessages.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Foris - web administration interface for OpenWrt based on NETCONF
+# Copyright (C) 2013 CZ.NIC, z.s.p.o. <www.nic.cz>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 for lang_dir in locale/*; do
 	locale_dir=$lang_dir/LC_MESSAGES/
 	for file in $locale_dir/*.po; do
