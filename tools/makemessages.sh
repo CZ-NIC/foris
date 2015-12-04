f8e92ee1a3467719c13225caed498f9d6ac124fc
Jan Čermák <jan.cermak@nic.cz>
2015-12-04 13:56:44 +0100

    tools: makemessages takes path to base directory as first arg

--- tools/makemessages.sh
+++ tools/makemessages.sh
@@ -15,17 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-if [ $# -eq 1 ]
-then
-	lang=$1
-	locale_dir="foris/locale/$lang/LC_MESSAGES"
+if [[ ! $# -eq 3 ]]; then
+	lang=$2
+	locale_dir="$1/locale/$lang/LC_MESSAGES"
 	if [ ! -d $locale_dir ]
 	then
 		mkdir -p $locale_dir
 	fi
-	find \( -iname "*.py" -o -iname "*.tpl" \) -exec cat {} \; | sed 's/"\?{{!* \(_\|trans\|\(\w*\)gettext\)\((".*")\).*}}"\?/\n\2gettext\3\n#/g' | \
+	find $1 \( -iname "*.py" -o -iname "*.tpl" \) -exec cat {} \; | sed 's/"\?{{!* \(_\|trans\|\(\w*\)gettext\)\((".*")\).*}}"\?/\n\2gettext\3\n#/g' | \
 		xgettext -d messages --no-location --language=Python --from-code=UTF-8 --output=$locale_dir/tmp_messages.pot -
-	find -iname "*.tpl" -exec sh -c 'grep -zq "%include.*_(" $0 && echo "WARNING: _() after %include in $0"' {} \;
+	find $1 -iname "*.tpl" -exec sh -c 'grep -zq "%include.*_(" $0 && echo "WARNING: _() after %include in $0"' {} \;
 	if [ -f $locale_dir/messages.po ]
 	then
 		echo "Making messages in $locale_dir."
@@ -36,5 +35,6 @@ then
 	fi
 	echo "Message making completed."
 else
-	echo "No language specified - use '$0 LANGUAGE'"
+    echo "$1 -- $2"
+	>&2 echo "Usage: $0 PATH LANGUAGE"
 fi
