110e94f6ee88fe3243204591fd3750be214e20c0
Jan Čermák <jan.cermak@nic.cz>
2016-03-31 13:59:48 +0200

    add hardcoded variable to determine type of the device
    This variable has to be set during the build process. It defaults
    to "turris".

--- foris/__init__.py
+++ foris/__init__.py
@@ -1,2 +1,4 @@
 # this file is automatically updated when distutils are running the setup
 __version__ = "unknown"
+# variable used to enable some device-specific features
+DEVICE_CUSTOMIZATION = "turris"  # should be either "turris" or "omnia"
