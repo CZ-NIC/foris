9cf4b2fc134af40d8360a983a857fcd87c224a5b
Jan Čermák <jan.cermak@nic.cz>
2016-05-11 16:58:45 +0200

    locale: add initial files for German translation

---
+++ foris/static/js/foris.de.js
@@ -0,0 +1,10 @@
+Foris.messages.qrErrorPassword = "Vámi zadané heslo obsahuje nestandardní znaky, které nejsou zakázané, avšak mohou na některých zařízeních způsobovat problémy.";
+Foris.messages.qrErrorSSID = "Vámi zadané jméno sítě obsahuje nestandardní znaky, které nejsou zakázané, avšak mohou na některých zařízeních způsobovat problémy.";
+Foris.messages.ok = "OK";
+Foris.messages.error = "Chyba";
+Foris.messages.loading = "Načítám...";
+Foris.messages.checkNoForward = "Test připojení se nezdařil, probíhá test připojení s vypnutým forwardováním.";
+Foris.messages.lanIpChanged = 'IP adresa Vašeho routeru se změnila. Router by měl nyní být dostupný na adrese <a href="%NEW_LOC%">%IP_ADDR%</a>. Více informací o změně IP adresy najdete výše.';
+Foris.messages.confirmRestart = "Opravdu chcete restartovat router?";
+Foris.messages.confirmRestartExtra = "\nZbývající nepřečtené zprávy (%UNREAD%) budou smazány.";
+Foris.messages.unsavedNotificationsAlert = "V nastavení byly provedeny změny, které nebyly uloženy.\nChcete změny zahodit a odeslat testovací zprávu s původním nastavením?";
