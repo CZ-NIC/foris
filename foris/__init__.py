2734774d0e18a56a0fefbe96d62dc98387b4008e
Jan Čermák <jan.cermak@nic.cz>
2016-09-19 13:12:22 +0200

    foris: make turris device customization the default one
    Again, I've accidentally committed omnia.

--- foris/__init__.py
+++ foris/__init__.py
@@ -1,4 +1,4 @@
 # this file is automatically updated when distutils are running the setup
 __version__ = "unknown"
 # variable used to enable some device-specific features
-DEVICE_CUSTOMIZATION = "omnia"  # should be either "turris" or "omnia"
+DEVICE_CUSTOMIZATION = "turris"  # should be either "turris" or "omnia"
