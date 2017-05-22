cdf26998a4d9a57b3d57f1fca9858ed00cf4ec6b
Stepan Henek <stepan.henek@nic.cz>
2017-05-22 16:12:32 +0200

    Guest network/wifi behaviour modified.
    * guest network setting moved to LAN tab
    * when guest network is disabled in LAN tab all guest wifis are disabled
    * when a guest wifi is enabled the guest network is enabled as well

--- foris/config_handlers/lan.py
+++ foris/config_handlers/lan.py
@@ -20,11 +20,21 @@ from foris.core import gettext_dummy as gettext, ugettext as _
 from foris.form import (
     Textbox, Checkbox
 )
-from foris.nuci.filters import create_config_filter
+from foris.nuci import client
+from foris.nuci.filters import create_config_filter, wifi_filter
+from foris.nuci.preprocessors import guest_network_enabled, generate_network_preprocessor
 from foris.nuci.modules.uci_raw import Uci, Config, Section, Option, List, Value
+from foris.utils.addresses import (
+    ip_num_to_str_4, ip_str_to_num_4, prefix_to_mask_4, mask_to_prefix_4
+)
 
 
 from .base import BaseConfigHandler
+
+
+DEFAULT_GUEST_NETWORK = "10.111.222.0"
+DEFAULT_GUEST_MASK = "255.255.255.0"
+DEFAULT_GUEST_PREFIX = mask_to_prefix_4(DEFAULT_GUEST_MASK)
 
 
 class LanHandler(BaseConfigHandler):
@@ -32,7 +42,7 @@ class LanHandler(BaseConfigHandler):
 
     def get_form(self):
         lan_form = fapi.ForisForm("lan", self.data,
-                                  filter=create_config_filter("dhcp", "network"))
+                                  filter=create_config_filter("dhcp", "network", "firewall"))
         lan_main = lan_form.add_section(
             name="set_lan",
             title=_(self.userfriendly_title),
@@ -65,6 +75,36 @@ class LanHandler(BaseConfigHandler):
                            nuci_path="uci.dhcp.lan.limit")\
             .requires("dhcp_enabled", True)
 
+        guest_network_section = lan_form.add_section(
+            name="guest_network",
+            title=_("Guest network"),
+        )
+        guest_network_section.add_field(
+            Checkbox, name="guest_network_enabled",
+            label=_("Enable guest network"), default=False,
+            hint=_(
+                "Is a network for guest devices. It is separated from your ordinary LAN network "
+                "and it is not possible to access the administration interface from the guest "
+                "network. The devices connected to the guest network are only allowed to access "
+                "the internet."
+            ),
+            nuci_preproc=guest_network_enabled,
+        )
+        guest_network_section.add_field(
+            Textbox, name="guest_network_subnet", label=_("Guest Network"),
+            nuci_preproc=generate_network_preprocessor(
+                "uci.network.guest_turris.ipaddr",
+                "uci.network.guest_turris.netmask",
+                DEFAULT_GUEST_NETWORK,
+                DEFAULT_GUEST_MASK,
+            ),
+            validators=[validators.IPv4Prefix()],
+            hint=_(
+                "You need to set the IP range of your guest network. Note that the range should "
+                "be different than the ranges on your other networks (LAN, WAN, OpenVPN, ...)."
+            ),
+        ).requires("guest_network_enabled", True)
+
         def lan_form_cb(data):
             uci = Uci()
             config = Config("dhcp")
@@ -90,8 +130,103 @@ class LanHandler(BaseConfigHandler):
             else:
                 dhcp.add(Option("ignore", "1"))
 
+            # update guest network configs
+            guest_enabled = data.get("guest_network_enabled")
+            guest_network_subnet = data.get("guest_network_subnet")
+            if guest_network_subnet:
+                network, prefix = data.get("guest_network_subnet").split("/")
+            else:
+                network, prefix = DEFAULT_GUEST_NETWORK, DEFAULT_GUEST_PREFIX
+            LanHandler.prepare_guest_configs(uci, guest_enabled, network, prefix)
+
+            # disable guest wifi when guest network is not enabled
+            if not guest_enabled:
+                wireless = uci.add(Config("wireless"))
+                data = client.get(filter=wifi_filter())
+                idx = 0
+                while data.find_child("uci.wireless.@wifi-device[%d]" % idx):
+                    guest_iface = wireless.add(Section("guest_iface_%d" % idx, "wifi-iface"))
+                    guest_iface.add(Option("disabled", "1"))
+                    idx += 1
+
             return "edit_config", uci
 
         lan_form.add_callback(lan_form_cb)
 
         return lan_form
+
+    @staticmethod
+    def prepare_guest_configs(uci, enabled, network, prefix):
+        ignore = "0" if enabled else "1"
+        enabled = "1" if enabled else "0"
+
+        # parse router ip address (192.168.1.0 -> 192.168.1.1)
+        router_ip = ip_num_to_str_4(ip_str_to_num_4(network) + 1)
+        netmask = prefix_to_mask_4(int(prefix))
+
+        # update network interface list
+        network_conf = uci.find_child("network") or Config("network")
+        uci.add(network_conf)
+        interface_section = Section("guest_turris", "interface")
+        network_conf.add_replace(interface_section)
+        interface_section.add(Option("enabled", enabled))
+        interface_section.add(Option("ifname", "guest_turris"))
+        interface_section.add(Option("proto", "static"))
+        interface_section.add(Option("ipaddr", router_ip))
+        interface_section.add(Option("netmask", netmask))
+
+        # update firewall config
+        firewall_conf = uci.find_child("firewall") or Config("firewall")
+        uci.add(firewall_conf)
+
+        zone_section = Section("guest_turris", "zone")
+        firewall_conf.add_replace(zone_section)
+        zone_section.add(Option("enabled", enabled))
+        zone_section.add(Option("name", "guest_turris"))
+        network_list = List("network")
+        network_list.add(Value(0, "guest_turris"))
+        zone_section.add(network_list)
+        zone_section.add(Option("input", "REJECT"))
+        zone_section.add(Option("forward", "REJECT"))
+        zone_section.add(Option("output", "ACCEPT"))
+
+        wan_forwarding_section = Section("guest_turris_forward_wan", "forwarding")
+        firewall_conf.add_replace(wan_forwarding_section)
+        wan_forwarding_section.add(Option("enabled", enabled))
+        wan_forwarding_section.add(Option("name", "guest to wan forward"))
+        wan_forwarding_section.add(Option("src", "guest_turris"))
+        wan_forwarding_section.add(Option("dest", "wan"))
+
+        dns_rule_section = Section("guest_turris_dns_rule", "rule")
+        firewall_conf.add_replace(dns_rule_section)
+        dns_rule_section.add(Option("enabled", enabled))
+        dns_rule_section.add(Option("name", "guest dns rule"))
+        dns_rule_section.add(Option("src", "guest_turris"))
+        dns_rule_section.add(Option("proto", "tcpudp"))
+        dns_rule_section.add(Option("dest_port", 53))
+        dns_rule_section.add(Option("target", "ACCEPT"))
+
+        dhcp_rule_section = Section("guest_turris_dhcp_rule", "rule")
+        firewall_conf.add_replace(dhcp_rule_section)
+        dhcp_rule_section.add(Option("enabled", enabled))
+        dhcp_rule_section.add(Option("name", "guest dhcp rule"))
+        dhcp_rule_section.add(Option("src", "guest_turris"))
+        dhcp_rule_section.add(Option("proto", "udp"))
+        dhcp_rule_section.add(Option("src_port", "67-68"))
+        dhcp_rule_section.add(Option("dest_port", "67-68"))
+        dhcp_rule_section.add(Option("target", "ACCEPT"))
+
+        # update dhcp config
+        dhcp_conf = uci.find_child("dhcp") or Config("dhcp")
+        uci.add(dhcp_conf)
+
+        dhcp_section = Section("guest_turris", "dhcp")
+        dhcp_conf.add(dhcp_section)
+        dhcp_section.add(Option("interface", "guest_turris"))
+        dhcp_section.add(Option("start", "200"))
+        dhcp_section.add(Option("limit", "50"))
+        dhcp_section.add(Option("leasetime", "1h"))
+        dhcp_section.add(Option("ignore", ignore))
+        dhcp_option_list = List("dhcp_option")
+        dhcp_option_list.add(Value(0, "6,%s" % router_ip))
+        dhcp_section.add(dhcp_option_list)
