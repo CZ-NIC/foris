49cb1c2358194e083623ba28eda76a1d5e5395b2
Stepan Henek <stepan.henek@nic.cz>
2018-02-07 09:38:22 +0100

    datetime translations fixes

--- foris/utils/template_helpers.py
+++ foris/utils/template_helpers.py
@@ -17,11 +17,11 @@
 import re
 
 from .routing import external_route
+from foris.utils.translators import _
 
 
 def shorten_text(text, max_chars, etc="..."):
-    """
-        Shortens text. "Very long text" -> "Very long..."
+    """ Shortens text. "Very long text" -> "Very long..."
     """
     if len(text) > max_chars:
         return text[:max_chars - len(etc)] + etc
@@ -34,3 +34,27 @@ def external_url(path):
 
 def remove_html_tags(text):
     return re.sub(r'<[^>]*>', '', text)
+
+
+def translate_datetime(datetime_instance, default="%Y/%m/%d %H:%M:%S"):
+    """ Tries to "translate" the datetime.
+
+    This functions should handle e.g. conversions between US / EU date format
+    according to localized translation. So these formats were added to be translated.
+    Unfortunatelly some translators used non-asci characters to format the date.
+    That would raise an exception and this function handles that by adding a fallback format.
+
+    :param datetime_instance: date to be formatted
+    :type datetime_instance: datetime.datetime
+    :param default: default format
+    :type default: str
+
+    :returns: formatted date
+    :rtype: str
+    """
+
+    try:
+        return datetime_instance.strftime(_("%Y/%m/%d %H:%M:%S"))
+    except UnicodeEncodeError:
+        # Unicode characters in translated format -> fallback to default
+        return datetime_instance.strftime(default)
