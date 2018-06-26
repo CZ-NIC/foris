5abe74a7cf64ea92dbb2ac7a492758f33644abda
Stepan Henek <stepan.henek@nic.cz>
2018-06-26 09:30:32 +0200

    version bump

--- foris/__init__.py
+++ foris/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 # this file is automatically updated when distutils are running the setup
-__version__ = "98.2"
+__version__ = "98.3"
 # variable used to enable some device-specific features
 DEVICE_CUSTOMIZATION = "turris"  # should be either "turris" or "omnia"
 
