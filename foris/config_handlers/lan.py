478e8ef356d6e4f78f77ed48a0f1e9e840a0abf1
Stepan Henek <stepan.henek@nic.cz>
2018-10-09 14:28:49 +0200

    lan: unmanaged mode added
    Unmanaged mode is a mode when the device connects to external networks
    via LAN. It is similar to WAN, but the device keeps all its ports
    opened. So it acts as a server in your LAN network.

--- foris/config_handlers/lan.py
+++ foris/config_handlers/lan.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from foris import fapi, validators
-from foris.form import Textbox, Checkbox, Number
+from foris.form import Textbox, Checkbox, Number, Dropdown
 from foris.state import current_state
 from foris.utils.routing import reverse
 from foris.utils.translators import gettext_dummy as gettext, _
@@ -30,13 +30,27 @@ class LanHandler(BaseConfigHandler):
     userfriendly_title = gettext("LAN")
 
     def get_form(self):
-        data = current_state.backend.perform("lan", "get_settings")
-        data["lan_ipaddr"] = data["ip"]
-        data["lan_netmask"] = data["netmask"]
-        data["dhcp_enabled"] = data["dhcp"]["enabled"]
-        data["dhcp_min"] = data["dhcp"]["start"]
-        data["dhcp_max"] = data["dhcp"]["limit"]
-        data["dhcp_leasetime"] = data["dhcp"]["lease_time"] // 60 // 60
+        backend_data = current_state.backend.perform("lan", "get_settings")
+        data = {}
+        data["mode"] = backend_data["mode"]
+        data["router_ip"] = backend_data["mode_managed"]["router_ip"]
+        data["router_netmask"] = backend_data["mode_managed"]["netmask"]
+        data["router_dhcp_enabled"] = backend_data["mode_managed"]["dhcp"]["enabled"]
+        data["router_dhcp_start"] = backend_data["mode_managed"]["dhcp"]["start"]
+        data["router_dhcp_limit"] = backend_data["mode_managed"]["dhcp"]["limit"]
+        data["router_dhcp_leasetime"] = backend_data["mode_managed"]["dhcp"]["lease_time"] \
+            // (60 * 60)
+        data["client_proto_4"] = backend_data["mode_unmanaged"]["lan_type"]
+        data["client_ip_4"] = backend_data["mode_unmanaged"]["lan_static"]["ip"]
+        data["client_netmask_4"] = backend_data["mode_unmanaged"]["lan_static"]["netmask"]
+        data["client_gateway_4"] = backend_data["mode_unmanaged"]["lan_static"]["gateway"]
+        dns1 = backend_data["mode_unmanaged"]["lan_static"].get("dns1")
+        if dns1:
+            data["client_dns1_4"] = dns1
+        dns2 = backend_data["mode_unmanaged"]["lan_static"].get("dns2")
+        if dns2:
+            data["client_dns2_4"] = dns2
+        data["client_hostname_4"] = backend_data["mode_unmanaged"]["lan_dhcp"].get("hostname", "")
 
         if self.data:
             # Update from post
@@ -59,42 +73,135 @@ class LanHandler(BaseConfigHandler):
         )
 
         lan_main.add_field(
-            Textbox, name="lan_ipaddr", label=_("Router IP address"),
+            Dropdown, name="mode", label=_("LAN mode"), args=[
+                ("managed", _("Managed")),
+                ("unmanaged", _("Unmanaged")),
+            ],
+            hint=_(
+                "Manage mode means that this devices manages the LAN "
+                "(acts as a router, can assing IP addresses, ...). "
+                "Unmanaged mode means that this device acts as a client in this network. "
+                "It acts in a similar way as WAN, but it has opened ports for configuration "
+                "interface and other services. "
+            ),
+            default="managed",
+        )
+
+        # managed options
+        lan_main.add_field(
+            Textbox, name="router_ip", label=_("Router IP address"),
             validators=validators.IPv4(),
             hint=_("Router's IP address in the inner network.")
-        )
+        ).requires("mode", "managed")
         lan_main.add_field(
-            Textbox, name="lan_netmask", label=_("Network netmask"),
+            Textbox, name="router_netmask", label=_("Network netmask"),
             validators=validators.IPv4Netmask(),
             hint=_("Network mask of the inner network.")
-        )
+        ).requires("mode", "managed")
         lan_main.add_field(
-            Checkbox, name="dhcp_enabled", label=_("Enable DHCP"),
+            Checkbox, name="router_dhcp_enabled", label=_("Enable DHCP"),
             preproc=lambda val: bool(int(val)), default=True,
             hint=_("Enable this option to automatically assign IP addresses to "
                    "the devices connected to the router.")
+        ).requires("mode", "managed")
+        lan_main.add_field(
+            Textbox, name="router_dhcp_start", label=_("DHCP start"),
+        ).requires("router_dhcp_enabled", True)
+        lan_main.add_field(
+            Textbox, name="router_dhcp_limit", label=_("DHCP max leases"),
+        ).requires("router_dhcp_enabled", True)
+        lan_main.add_field(
+            Textbox, name="router_dhcp_leasetime", label=_("Lease time (hours)"),
+            validators=[validators.InRange(1, 7 * 24)]
+        ).requires("router_dhcp_enabled", True)
+
+        # unmanaged options
+        LAN_DHCP = "dhcp"
+        LAN_STATIC = "static"
+        LAN_NONE = "none"
+        LAN_OPTIONS = (
+            (LAN_DHCP, _("DHCP (automatic configuration)")),
+            (LAN_STATIC, _("Static IP address (manual configuration)")),
+            (LAN_NONE, _("Don't connect this device to LAN")),
         )
         lan_main.add_field(
-            Textbox, name="dhcp_min", label=_("DHCP start"),
-        ).requires("dhcp_enabled", True)
+            Dropdown, name="client_proto_4", label=_("IPv4 protocol"), args=LAN_OPTIONS,
+            default=LAN_DHCP
+        ).requires("mode", "unmanaged")
+        # unmanaged static
         lan_main.add_field(
-            Textbox, name="dhcp_max", label=_("DHCP max leases"),
-        ).requires("dhcp_enabled", True)
+            Textbox, name="client_ip_4", label=_("IPv4 address"), required=True,
+            validators=validators.IPv4()
+        ).requires("client_proto_4", LAN_STATIC)
         lan_main.add_field(
-            Textbox, name="dhcp_leasetime", label=_("Lease time (hours)"),
-            validators=[validators.InRange(1, 7 * 24)]
-        ).requires("dhcp_enabled", True)
+            Textbox, name="client_netmask_4", label=_("Network mask"), required=True,
+            validators=validators.IPv4Netmask()
+        ).requires("client_proto_4", LAN_STATIC)
+        lan_main.add_field(
+            Textbox, name="client_gateway_4", label=_("Gateway"), required=True,
+            validators=validators.IPv4(),
+        ).requires("client_proto_4", LAN_STATIC)
+        lan_main.add_field(
+            Textbox, name="client_dns1_4", label=_("DNS server 1 (IPv4)"),
+            validators=validators.IPv4(),
+            hint=_(
+                "DNS server address is not required as the built-in "
+                "DNS resolver is capable of working without it."
+            )
+        ).requires("client_proto_4", LAN_STATIC)
+        lan_main.add_field(
+            Textbox, name="client_dns2_4", label=_("DNS server 2 (IPv4)"),
+            validators=validators.IPv4(),
+            hint=_(
+                "DNS server address is not required as the built-in "
+                "DNS resolver is capable of working without it."
+            )
+        ).requires("client_proto_4", LAN_STATIC)
+        # unamanaged dhcp
+        lan_main.add_field(
+            Textbox, name="client_hostname_4", label=_("DHCP hostname"),
+            validators=validators.Domain(),
+            hint=_(
+                "Hostname which will be provided to DHCP server."
+            )
+        ).requires("client_proto_4", LAN_DHCP)
 
         def lan_form_cb(data):
-            msg = {
-                "ip": data["lan_ipaddr"],
-                "netmask": data["lan_netmask"],
-                "dhcp": {"enabled": data["dhcp_enabled"]},
-            }
-            if data["dhcp_enabled"]:
-                msg["dhcp"]["start"] = int(data["dhcp_min"])
-                msg["dhcp"]["limit"] = int(data["dhcp_max"])
-                msg["dhcp"]["lease_time"] = int(data["dhcp_leasetime"]) * 60 * 60
+            msg = {"mode": data["mode"]}
+            if msg["mode"] == "managed":
+                dhcp = {
+                    "enabled": data["router_dhcp_enabled"],
+                }
+                if dhcp["enabled"]:
+                    dhcp["start"] = data["router_dhcp_start"]
+                    dhcp["limit"] = data["router_dhcp_limit"]
+                    dhcp["lease_time"] = int(data.get("router_dhcp_leasetime", 12)) * 60 * 60
+                msg["mode_managed"] = {
+                    "router_ip": data["router_ip"],
+                    "netmask": data["router_netmask"],
+                    "dhcp": dhcp,
+                }
+            elif data["mode"] == "unmanaged":
+                msg["mode_unmanaged"] = {
+                    "lan_type": data["client_proto_4"],
+                }
+                if data["client_proto_4"] == "static":
+                    msg["mode_unmanaged"]["lan_static"] = {
+                        "ip": data["client_ip_4"],
+                        "netmask": data["client_netmask_4"],
+                        "gateway": data["client_gateway_4"],
+                    }
+                    dns1 = data.get("client_dns1_4")
+                    if dns1:
+                        msg["mode_unmanaged"]["lan_static"]["dns1"] = dns1
+
+                    dns2 = data.get("client_dns2_4")
+                    if dns2:
+                        msg["mode_unmanaged"]["lan_static"]["dns2"] = dns2
+
+                elif data["client_proto_4"] == "dhcp":
+                    hostname = data.get("client_hostname_4")
+                    msg["mode_unmanaged"]["lan_dhcp"] = {"hostname": hostname} if hostname else {}
 
             res = current_state.backend.perform("lan", "update_settings", msg)
             return "save_result", res  # store {"result": ...} to be used later...
