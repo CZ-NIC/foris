40a458eb8bac884ea924323005e063c6d13b219a
Stepan Henek <stepan.henek@nic.cz>
2019-04-01 11:35:03 +0200

    sample plugin updated: reformatted and dependency_link added

--- examples/sample_plugin/setup.py
+++ examples/sample_plugin/setup.py
@@ -10,6 +10,7 @@ class BuildCmd(build_py):
     def run(self):
         # build foris plugin files
         from foris_plugins_distutils import build
+
         cmd = build(copy.copy(self.distribution))
         cmd.ensure_finalized()
         cmd.run()
@@ -26,23 +27,12 @@ setup(
     author_email="stepan.henek@nic.cz",
     url="https://gitlab.labs.nic.cz/turris/foris-sample-plugin/",
     license="GPL-3.0",
-    install_requires=[
-        "foris",
-        'jinja2',
-    ],
-    setup_requires=[
-        'babel',
-        'libsass',
-        'foris_plugins_distutils',
-    ],
-    provides=[
-        "foris_plugins.sample",
-    ],
-    packages=[
-        "foris_plugins.sample",
-    ],
+    install_requires=["foris", "jinja2"],
+    setup_requires=["babel", "libsass", "foris_plugins_distutils"],
+    provides=["foris_plugins.sample"],
+    packages=["foris_plugins.sample"],
     package_data={
-        '': [
+        "": [
             "templates/**",
             "templates/**/*",
             "templates/javascript/**",
@@ -53,12 +43,12 @@ setup(
             "static/img/*",
             "static/js/*.js",
             "static/js/contrib/*",
-        ],
+        ]
     },
-    namespace_packages=[
-        'foris_plugins',
+    namespace_packages=["foris_plugins"],
+    cmdclass={"build_py": BuildCmd},  # modify build_py to build the foris files as well
+    dependency_links=[
+        "git+https://gitlab.labs.nic.cz/turris/foris-plugins-distutils.git"
+        "#egg=foris_plugins_distutils"
     ],
-    cmdclass={
-        "build_py": BuildCmd,  # modify build_py to build the foris files as well
-    }
 )
